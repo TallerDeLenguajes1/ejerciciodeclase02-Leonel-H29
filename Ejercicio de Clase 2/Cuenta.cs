47190dd1ee2bb5cd15b64470d5c12a6034c85852
Leonel-H29 <leonelherreradkno@gmail.com>
2021-09-08 18:44:39 -0300

    Creo las funciones

--- Ejercicio de Clase 2/Cuenta.cs
+++ Ejercicio de Clase 2/Cuenta.cs
@@ -11,20 +11,123 @@ namespace Ejercicio_de_Clase_2
     {
         int num;
         TipoDeExtraccion ext;
-        public Cuenta() { }
+        public Cuenta() {
+            this.num = 0;
+        }
         public Cuenta(int _num, TipoDeExtraccion extra) {
             this.num = _num;
             this.ext = extra;
         }
         public int insersion(int num) {
-            return;
+            return 0;
         }
-        public int extraccion(int num, TipoDeExtraccion extra) { return; }
+        public int extraccion(int num, TipoDeExtraccion extra) { return 0; }
     }
 
-    public class Cta_Cte_Pesos : Cuenta { }
+    public class Cta_Cte_Pesos : Cuenta {
 
-    public class Cta_Cte_Dolares : Cuenta { }
+        public int insersion(int num)
+        {
+            return 0;
+        }
+        public int extraccion(int num, TipoDeExtraccion extra) { 
+           if(extra == CajeroHumano)
+            {
+                if(num >= 20000)
+                {
+                    Console.WriteLine(@"Se extrajo {num}");
+                    return num;
+                }
+                else {
+                    Console.WriteLine(@"No puede extraer ese dinero");
+                    return 0;
+                }
+            }
+            else {
+                if (num >= 20000)
+                {
+                    Console.WriteLine(@"Se extrajo {num}");
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine(@"No puede extraer ese dinero");
+                    return 0;
+                }
+            }
+        }
+    }
 
-    public class Caja_Ahorro_Pesos : Cuenta { }
+    public class Cta_Cte_Dolares : Cuenta {
+        public int insersion(int num)
+        {
+            return 0;
+        }
+        public int extraccion(int num, TipoDeExtraccion extra)
+        {
+            if (extra == CajeroHumano)
+            {
+                if (num >= 20000)
+                {
+                    Console.WriteLine(@"Se extrajo {num}");
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine(@"No puede extraer ese dinero");
+                    return 0;
+                }
+            }
+            else
+            {
+                if (num >= 20000)
+                {
+                    Console.WriteLine(@"Se extrajo {num}");
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine(@"No puede extraer ese dinero");
+                    return 0;
+                }
+            }
+        }
+
+    }
+
+    public class Caja_Ahorro_Pesos : Cuenta {
+        public int insersion(int num)
+        {
+            return 0;
+        }
+        public int extraccion(int num, TipoDeExtraccion extra)
+        {
+            if (extra == CajeroHumano)
+            {
+                if (num >= 20000)
+                {
+                    Console.WriteLine(@"Se extrajo {num}");
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine(@"No puede extraer ese dinero");
+                    return 0;
+                }
+            }
+            else
+            {
+                if (num >= 20000)
+                {
+                    Console.WriteLine(@"Se extrajo {num}");
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine(@"No puede extraer ese dinero");
+                    return 0;
+                }
+            }
+        }
+    }
 }
