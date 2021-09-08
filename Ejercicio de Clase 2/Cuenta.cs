e7f1cff8f3c191d86db542ad17d25af2fd235eb7
Leonel-H29 <leonelherreradkno@gmail.com>
2021-09-08 17:51:00 -0300

    Creo las clases

---
+++ Ejercicio de Clase 2/Cuenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Clase_2
+{
+    public enum TipoDeExtraccion {CajeroHumano =1,CajeroAutomatico };
+    public class Cuenta
+    {
+        int num;
+        TipoDeExtraccion ext;
+        public Cuenta() { }
+        public Cuenta(int _num, TipoDeExtraccion extra) {
+            this.num = _num;
+            this.ext = extra;
+        }
+        public int insersion(int num) {
+            return;
+        }
+        public int extraccion(int num, TipoDeExtraccion extra) { return; }
+    }
+
+    public class Cta_Cte_Pesos : Cuenta { }
+
+    public class Cta_Cte_Dolares : Cuenta { }
+
+    public class Caja_Ahorro_Pesos : Cuenta { }
+}
---
+++ Ejercicio de Clase 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_de_Clase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
